36598c4df667d8676494e2859f03b201d78a9ffe
Luiz <luiz308araujofarias@gmail.com>
2026-03-22 10:13:07 -0300

    Criando Repositorio

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include<iostream> // Biblioteca de Entrada e Saída
+using namespace std; // Funções padrão do C++
+
+// Função Principal
+int main(){
+    cout << "Hello World" << endl;  
+
+    string name = "Shaolin Matador de Porcos"; 
+    cout << "Welcome, " << name << endl;
+
+    int secretNumber = 42;
+    cout << "O número secreto é " << secretNumber << ". Não conte para ninguém!" << endl;
+
+    return 0;
+}
