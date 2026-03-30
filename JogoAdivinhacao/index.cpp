b5ccc43997a2b9abdf2546089300991a0bf76a46
Luiz <luiz308araujofarias@gmail.com>
2026-03-30 19:40:44 -0300

    Finalizando Jogo Adivinhação

--- JogoAdivinhacao/index.cpp
+++ JogoAdivinhacao/index.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
 
+    srand(time(NULL));
+
+    // CONSTANTE
+    const int secretNumber = rand() % 100;
+    cout << "O numero secreto é " << secretNumber << endl;
+
     // COUT COMANDO DE SAIDA
     cout << "--* Bem-Vindo ao jogo da adivinhação *--" << endl;
     cout << "     -- Você tem 10 tentativas! --" << endl;
-
-
-    // CONSTANTE
-    const int secretNumber = 42;
     
-    int totalAttempts = 10;    
+    cout << "\nEscolha o seu nível de dificuldade: " << endl;
+    cout << "Fácil (F), Médio (M) ou Difícil (D)" << endl;
+
+    char diff;
+    cin >> diff;
+
+    int totalAttempts;
+
+    if (diff == 'F'){
+        totalAttempts = 15;
+    }
+    else if (diff == 'M'){
+        totalAttempts = 10;
+    }
+    else if(diff == 'D'){
+        totalAttempts = 5;
+    } 
+
+    int currentAttempt = totalAttempts;
+
+    cout << "   \nVocê terá " << totalAttempts << " tentativas." << endl;
 
     while(true){
 
-        totalAttempts--;
+        currentAttempt--;    
 
         int attempt;
         cout << "\nQual seu chute?: ";
@@ -26,7 +50,7 @@ int main() {
         bool below = attempt < secretNumber;
         bool equal = attempt == secretNumber;
 
-        if(totalAttempts == 0){
+        if(currentAttempt == 0){
             cout << "\nSuas tentativas acabaram!" << endl;
             cout << "   - Você perdeu!" << endl;
             break;
@@ -34,17 +58,17 @@ int main() {
 
         if(equal){
             cout << "\nParabéns! Você acertou o número secreto!" << endl;
-            cout << "   - Você acertou em " << 10 - totalAttempts << " tentativas" << endl;
+            cout << "   - Você acertou em " << totalAttempts - currentAttempt << " tentativas" << endl;
             break;
         }
         else if(above){
             cout << "\nSeu chute foi maior que o número secreto!" << endl;
-            cout << "   - Lhe restam " << totalAttempts << " tentativas!" << endl;
+            cout << "   - Lhe restam " << currentAttempt << " tentativas!" << endl;
             cout << "\nTente novamente: ";
         }
         else{
             cout << "\nSeu chute foi menor que o número secreto!" << endl;
-            cout << "   - Lhe restam " << totalAttempts << " tentativas!" << endl;
+            cout << "   - Lhe restam " << currentAttempt << " tentativas!" << endl;
             cout << "\nTente novamente: ";
         }
     }
