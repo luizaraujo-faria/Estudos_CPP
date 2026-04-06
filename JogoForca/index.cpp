2d5e3765bf73d39ada17d4589dc9984fcb4ad67a
Luiz <luiz308araujofarias@gmail.com>
2026-04-06 17:30:42 -0300

    Finalizando Jogo da Forca

--- JogoForca/index.cpp
+++ JogoForca/index.cpp
@@ -1,52 +1,158 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <iostream> // LIB PARA ENTRADA E SAÍDA DE DADOS NO CMD
+#include <string>   // LIB PARA UTILIZAR O TIPO STRING
+#include <map>  // LIB PARA ADICIONAR DICIONARIO "MAP"
+#include <vector>   // LIB PARA ADICIONAR O VECTOR "VETOR DINAMICO"
+#include <fstream> // LIB PARA ADICIONAR O IFSTREAM "MANIPULAR ARQUIVOS"
+#include <ctime>
+#include <cstdlib>
+using namespace std;    // LIB PADRÃO DO C++
 
+// char secretWord[20]; --> MANEIRA DE DECLARAR UMA STRING
+// cout << "A palavra secreta é: " << secretWord << endl;
+
+char attempt;
 // VARIAVEL EM ESCOPO GLOBAL
-const string SECRET_WORD = "Melancia";
+string secretWord;
+// TOTAL DE TENTATIVAS
+int totalAttempts = 5;
+// DICIONÁRIO PARA MAPEAR AS LETRAS DA PALAVRA SECRETA
+map<char, bool> wordLetters;
+//  VETOR DINAMICO PARA SALVAR OS CHUTES ERRADOS
+vector<char> badAttempts;
+
+
+//  LE O TXT COM AS PALAVRAS E AS LISTA
+vector<string> readWordFile() {
+    vector<string> wordsInFile;
+
+    ifstream file;
+    file.open("words.txt"); // ABRE O ARQUIVO
+
+    //  VERIFICA SE O ARQUIVO FOI ABERTO E LIDO CORRETAMENTE
+    if(file.is_open()){
+        int totalWords;
+        file >> totalWords; // PEGA O PRIMEIRO ELEMENTO DO ARQUIVO
+
+        // cout << "O jogo possui " << totalWords << " palavras.\n" << endl;
+
+        for(int i = 0; i < totalWords; i++){
+            string readedWord;
+            file >> readedWord;
+            // cout << i + 1 << "-" << readedWord << endl;
+            wordsInFile.push_back(readedWord);
+        }
+
+        file.close();   // FECHA O ARQUIVO
+        return wordsInFile;
+    }
+    else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void randomWord() {
+    vector<string> words = readWordFile();
+
+    srand(time(NULL));
+    int randomIndex = rand() % words.size();
+
+    secretWord = words[randomIndex];
+    // cout << "Palavra sorteada " << secretWord;
+}
+
+void setStart(){
+    randomWord();
+    cout << "\n****************************************" << endl;
+    cout << "****** Bem-Vindo ao Jogo da Forca ******" << endl;
+    cout << "****************************************" << endl;
+    cout << "\n--- Você terá um total de " << totalAttempts << " tentativas ---" << endl;
+}
+
+void makeAttempt() {
+    // char attempt;
+    cout << "\nChute uma letra: ";
+    cin >> attempt;
+    wordLetters[attempt] = true;
+}
 
 bool letterExists(char attempt){
-
-    for(int i = 0; i < SECRET_WORD.size(); i++){
-        if(attempt == SECRET_WORD[i]){
+    // C++ 11 - MANEIRA MAIS MODERNA
+    for(char letter : secretWord){
+        if(attempt == letter){
+            cout << "\nVocê acertou, a letra '" << attempt << "' está na palavra!" << endl;
+            cout << "Ainda lhe restam " << totalAttempts << " tentativas!" << endl;
             return true;
         }
     }
 
+    cout << "\nVocê errou, a letra '" << attempt << "' não está na palavra!" << endl;
+    cout << "Lhe restam " << --totalAttempts << " tentativas!" << endl;
     return false;
 }
 
-int main() {
+void showWordState() {
+    cout << endl;
+    cout << "*** PALAVRA: ";
+    //  ITERA POR CADA LETRA DA PALAVRA
+    for(char letter : secretWord){
+        // VERIFICA SE A LETRA EXISTE
+        if(wordLetters[letter]){
+            // SE SIM, MARCA VERDADEIRO NO DICIONÁRIO E IMPRIME A LETRA NA TELA
+            cout << letter << " ";
+        }
+        else{
+            // CASO CONTRÁRIO EXIBE LINHA VAZIA ṔARA CADA LETRA
+            cout << "_ ";
+        }
+    }
+    cout << endl;
+}
+
+void showBadAttempts() {
+    cout << "*** CHUTES ERRADOS: ";
+    for(char letter : badAttempts){
+        cout << letter << " ";
+    }
+    cout << endl;
+}
+
+bool incompletedWord(){
+    for(char letter : secretWord){
+        if(!wordLetters[letter]){
+            return true;
+        }
+    }
+
+    cout << "\n***** PARABÉNS, VOCÊ ACERTOU A PALAVRA SECRETA *****" << endl;
+    cout << "A PALAVRA SECRETA ERA --- " << secretWord << " ---" << endl;
+    return false;
+}
+
+bool gameOver() {
+    if(badAttempts.size() == 5){
+        cout << "\n***** GAME OVER *****" << endl;
+        cout << "SUAS CHANCES ACABARAM, VOCÊ ENFORCOU!" << endl;
+        return true;
+    }
     
-    // char SECRET_WORD[20]; --> MANEIRA DE DECLARAR UMA STRING
-    // cout << "A palavra secreta é: " << SECRET_WORD << endl;
+    return false;
+}
 
-    cout << "--* Bem-Vindo ao Jogo da Forca *--" << endl;
 
-    int totalAttempts = 10;
+int main() {
 
-    while(true){
+    setStart();
+    while(incompletedWord()){
 
-        totalAttempts--;
+        showWordState();
+        showBadAttempts();
+        makeAttempt();
 
-        char attempt;
-        cout << "\nChute uma letra: " << endl;
-        cin >> attempt;
-        cout << "Seu chute foi: " << attempt << endl;
-
-        if(letterExists(attempt)){
-            cout << "\nVocê acertou, a letra " << attempt << " está na palavra!" << endl;
-            cout << "Lhe restam " << 10 - totalAttempts << " tentativas!" << endl;
-        }
-
-        if(!letterExists(attempt)){
-            cout << "\nVocê errou, a letra " << attempt << " não está na palavra!" << endl;
-            cout << "Lhe restam " << 10 - totalAttempts << " tentativas!" << endl;
-        }
+        if(!letterExists(attempt))
+            badAttempts.push_back(attempt);
         
-        if(totalAttempts == 0){
-            cout << "\nSuas chances acabaram, voce enforcou!" << endl;
+        if(gameOver())
             break;
-        }
     }
 }
